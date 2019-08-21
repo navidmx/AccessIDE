99301e855a0bcbc596f3fc15455c6d853e15eee0
Garrett Burroughs <garrettburroughs02@gmail.com>
2019-08-21 16:22:07 -0400

    nav searching implemented

--- server/src/languages/javascript/nav.ts
+++ server/src/languages/javascript/nav.ts
@@ -1,13 +1,33 @@
 import { Nav } from "../language";
 
 class JSNav implements Nav {
-    findFunction() {
-        throw new Error("Method not implemented.");
-    }
+    nav(command: string, lines: string[]): string {
+        const checkpoints: point[] = [];
+        const functions: point[] = [];
 
-    nav(): string {
+        for (let i = 0; i < lines.length; i++) {
+            if (lines[i].includes('// ~')) checkpoints.push({ name: lines[i].replace('// ~', ''), line: i });
+            if (lines[i].includes('function ')) checkpoints.push({ name: lines[i].split(' ')[1].split('(')[0], line: i });
+        }
+
+        if (command.includes('checkpoint')) {
+            const keyword = command.substring(command.indexOf('checkpoint') + 11);
+            
+        }
+        if (command.includes('line')) {
+
+        }
+        if (command.includes('function')) {
+
+        }
         return "";
     }
+}
+
+interface point {
+    name: string,
+    line: number
+
 }
 
 export default new JSNav();
--- server/src/languages/language.ts
+++ server/src/languages/language.ts
@@ -1,23 +1,22 @@
 export interface Write {
-    createFunction(tabs : number, name : string, parameters : string[]) : string;
-    createVariable(name : string, value : any) : string;
-    createConstant(name : string, value : any) : string;
-    createForLoop(tabs : number, start : number, end : number, step : number, flag : string) : string;
-    createWhileLoop(tabs : number, condition : string) : string;
-    createIfStatement(tabs : number, condition : string) : string;
-    createElseIfStatement(tabs : number, condition : string) : string;
-    createElseStatement(tabs : number) : string;
-    write(input : string, tabs : number, line : number) : {
+    createFunction(tabs: number, name: string, parameters: string[]): string;
+    createVariable(name: string, value: any): string;
+    createConstant(name: string, value: any): string;
+    createForLoop(tabs: number, start: number, end: number, step: number, flag: string): string;
+    createWhileLoop(tabs: number, condition: string): string;
+    createIfStatement(tabs: number, condition: string): string;
+    createElseIfStatement(tabs: number, condition: string): string;
+    createElseStatement(tabs: number): string;
+    write(input: string, tabs: number, line: number): {
         cmd: string,
         audio: string
     };
 }
 
 export interface Read {
-    readLine(line : string) : string;
+    readLine(line: string): string;
 }
 
 export interface Nav {
-    findFunction(line : string);
-    nav(input : string) : string;
+    nav(command: string, lines: string[]): string;
 }
